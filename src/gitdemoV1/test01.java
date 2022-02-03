4b573e82e291356006aa27678ba10766198e07fb
pk5121 <apraveenkumar51@gmail.com>
2022-02-03 20:08:50 +0530

    first try commit

---
+++ src/gitdemoV1/test01.java
@@ -0,0 +1,16 @@
+package gitdemoV1;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hun = "y";
+		if(hun == "y")
+			System.out.println("eat something......");
+		else {
+			System.out.println("Do work.......");
+		}
+
+	}
+
+}
